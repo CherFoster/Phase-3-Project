bb938d1a32a62307f5633d1da671975b55db978c
Cherise Foster <cherise.foster@outlook.com>
2023-07-10 13:10:17 -0600

    updated debug.py

---
+++ lib/debug.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == '__main__':
+    engine = create_engine("sqlite:///flights.db")
+    session = Session(engine, future=True)
+
+    import ipdb; ipdb.set_trace()
