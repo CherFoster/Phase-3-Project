1fbfe05d4427ef24c4ea8a096e7292e4a1a7719f
Cherise Foster <cherise.foster@outlook.com>
2023-07-14 13:18:24 -0600

    added to cli.py to look up flight information for each airline

--- lib/cli.py
+++ lib/cli.py
@@ -9,21 +9,76 @@ session = Session(engine, future=True)
 
 def greeting():
     print("")
-    print(Figlet(font = "starwars").renderText("Flight Info"))
+    print(Figlet(font = "cyberlarge").renderText("Flight Info"))
+
+def navigate_back():
+    selection = 0
+    while selection != 1:
+        print('''
+                Would you like to do another search?
+                    1) Yes
+                    2) No - Go back to Main Menu
+              ''')
+        selection = int(input("               Enter number : ")) 
+
+        if selection == 1:
+            print("")
+
+        if selection == 2:
+            main()
+
 
 def main():
     choice = 0
     while choice != 4:
-        print("Welcome...")
+        print("                Welcome Aboard")
         print('''
-                What information would you like to look up?
+                What information are you looking for?
+              
                     1) Flights
                     2) Passenger Information
                     3) Reservations
-                    4) Exit 
+                    4) Deplane (Exit) 
               ''')
-        choice = int(input("                Enter : ")) 
+        choice = int(input("               Enter number : ")) 
 
-if __name__ == 'main':
+        if choice == 1:
+            flight_choice = 0
+            while flight_choice != 4:
+                print(Figlet(font = "standard").renderText("Flights"))
+                print('''
+                    1) View all flights
+                    2) Search by airline
+                    3) Search by origin
+                    4) Search by destination  
+                      ''')
+                flight_choice = int(input("               Enter number : "))
+                all_flights = session.query(Flight).all()
+                if flight_choice == 1:
+                    # Set() keeps track of airline names already printed
+                    all_airlines = set() 
+                    for flight in all_flights:
+                        if flight.airline not in all_airlines:
+                            print(flight.airline)
+                            all_airlines.add(flight.airline)
+                    print("")
+                    view_airline = input("Enter the name of the airline : ")
+                    print("")
+                    airline_name = session.query(Flight).filter(Flight.airline.ilike(f"%{view_airline}%")).all()
+                    
+                    if airline_name:
+                        for info in airline_name:
+                            print(f"Flight Number: {info.flight_number}")
+                            print(f"Origin: {info.origin}")
+                            print(f"Destination: {info.destination}")
+                            print(f"Departure Time: {info.departure_time}")
+                            print(f"Arrival Time: {info.arrival_time}")
+                            print("")
+
+                    navigate_back()
+                            
+                        
+
+if __name__ == '__main__':
     greeting()
     main()
